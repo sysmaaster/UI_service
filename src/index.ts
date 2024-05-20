0e20170bdd1d9258f4ce115e44e93c9f5b6a8ffe
sysmaaster <sysmaaster@gmail.com>
2024-05-20 09:23:18 +0300

    docker compose

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { createServer } from "http";
 
 function startServer() {
   const server = createServer(app);
-  const port: number =  80;
+  const port: number =  4000;
   server.listen(port, async () => {
     logger.info(`Example App listening at http://localhost:${port}`);
   });
